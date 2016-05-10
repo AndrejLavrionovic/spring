5f6f13e9a9e1e5091ff1c85dfa85426d73a22cb4
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-05-10 21:01:38 +0100

    altech (After) -> AmsMessage is modified.

--- altech-mgmt-system/src/ie/gmit/sw/dao/AmsMessage.java
+++ altech-mgmt-system/src/ie/gmit/sw/dao/AmsMessage.java
@@ -7,41 +7,44 @@ import javax.validation.constraints.NotNull;
 public class AmsMessage {
 
 	private int msgid;
-	private int empnum;
 	private Date date;
 	private String msg;
+	private User user;
 	
 	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 	// CONSTRUCRTORS
 	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 	public AmsMessage(){}
 	
-	public AmsMessage(int msgid, int empnum, Date date, String msg) {
-		this.msgid = msgid;
-		this.empnum = empnum;
+	public AmsMessage(String msg, User user){
+		this.msg = msg;
+		this.user = user;
+	}
+	
+	public AmsMessage(Date date, String msg, User user){
 		this.date = date;
 		this.msg = msg;
+		this.user = user;
+	}
+	
+	public AmsMessage(int msgid, Date date, String msg, User user) {
+		this.msgid = msgid;
+		this.date = date;
+		this.msg = msg;
+		this.user = user;
 	}
 	
 
 	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 	// GETTERS/SETTERS
 	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
+	
 	public int getMsgid() {
 		return msgid;
 	}
 
 	public void setMsgid(int msgid) {
 		this.msgid = msgid;
-	}
-
-	public int getEmpnum() {
-		return empnum;
-	}
-
-	public void setEmpnum(int empnum) {
-		this.empnum = empnum;
 	}
 
 	public Date getDate() {
@@ -59,5 +62,64 @@ public class AmsMessage {
 	public void setMsg(String msg) {
 		this.msg = msg;
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 	
+
+	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+	// HASHCODE/EQUALS
+	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((msg == null) ? 0 : msg.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AmsMessage other = (AmsMessage) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (msg == null) {
+			if (other.msg != null)
+				return false;
+		} else if (!msg.equals(other.msg))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	
+
+	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+	// TOSTRING
+	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+	@Override
+	public String toString() {
+		return "AmsMessage [msgid=" + msgid + ", date=" + date + ", msg=" + msg + ", user=" + user + "]";
+	}
 }
