95ed337f1575c9289a105eeb705a5263782993c2
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-06-06 22:14:20 +0100

    altech (Continue) -> JUnit(dao): UserDao methods are tested.

--- altech-mgmt-system/src/ie/gmit/sw/dao/User.java
+++ altech-mgmt-system/src/ie/gmit/sw/dao/User.java
@@ -138,4 +138,64 @@ public class User {
 		return "User [username=" + username + ", email=" + email + ", empnum=" + empnum + ", firstname=" + firstname
 				+ ", lastname=" + lastname + ", tel=" + tel + ", authority=" + authority + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((authority == null) ? 0 : authority.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + empnum;
+		result = prime * result + (enabled ? 1231 : 1237);
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + tel;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (authority == null) {
+			if (other.authority != null)
+				return false;
+		} else if (!authority.equals(other.authority))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (empnum != other.empnum)
+			return false;
+		if (enabled != other.enabled)
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (tel != other.tel)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+	
 }
--- altech-mgmt-system/test/ie/gmit/sw/test/tests/UserDaoTests.java
+++ altech-mgmt-system/test/ie/gmit/sw/test/tests/UserDaoTests.java
@@ -1,12 +1,23 @@
 package ie.gmit.sw.test.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ie.gmit.sw.dao.User;
+import ie.gmit.sw.dao.UserDAO;
 
 @ActiveProfiles("dev")
 @ContextConfiguration(locations = {
@@ -15,9 +26,32 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 		"classpath:ie/gmit/sw/test/config/datasource.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserDaoTests {
+	
+	@Autowired
+	private UserDAO userdao;
+	
+	@Autowired
+	private DataSource datasource;
 
+	@Before
+	public void init(){
+		JdbcTemplate jdbc = new JdbcTemplate(datasource);
+
+		jdbc.execute("delete from systemmsg");
+		jdbc.execute("delete from users");
+	}
+	
 	@Test
 	public void createUserTest(){
-		assertEquals("Dummy Test", 1, 1);
+		
+		User user = new User("user", "user", true, "user@user.com", 100001, "Andrej", "Andrej", 861234567, "admin");
+		assertTrue("Creation user should return true", userdao.createUser(user));
+
+		List<User> users = userdao.getAllUsers();
+		assertEquals("Number of users should be 1", 1, users.size());
+		
+		assertTrue("User should exist", userdao.exists(user.getUsername()));
+		
+		assertEquals("Created user should be adentical to retrieved user", user, users.get(0));
 	}
 }
