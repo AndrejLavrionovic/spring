9bb5e7672579438b3e1bc1304b134984a278c04a
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-06-28 13:09:00 +0100

    altech (Continue) -> User corrected.

--- altech-mgmt-system/src/ie/gmit/sw/dao/User.java
+++ altech-mgmt-system/src/ie/gmit/sw/dao/User.java
@@ -2,7 +2,6 @@ package ie.gmit.sw.dao;
 
 import java.sql.Date;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
